74dc3fd8eedcee4d675c9afcde7847ec2ef9d20d
Seohee  Im <seoheeim@vt.edu>
2020-07-22 15:56:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def monopoly_with_third_tier
   monopoly = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {one_piece_owned: "25", },
+      :rent_in_dollars => {one_piece_owned: "25", two_piece owned: "50", three_piece_owned: "100", four_pieces_owned: "200"},
       :names => {}
     }
   }
