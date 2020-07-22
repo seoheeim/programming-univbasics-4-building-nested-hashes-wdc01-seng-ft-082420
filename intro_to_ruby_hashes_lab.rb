71893556801c03e90dc9ec938e08e3bd0568b60e
Seohee  Im <seoheeim@vt.edu>
2020-07-22 16:20:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -55,8 +55,9 @@ def monopoly_with_fourth_tier
     {
       :reading_railroad=>
       {
-        "mortgage_value=>"$100"
+        "mortgage_value"=>"$100"
       },  
+      
       :pennsylvania_railroad=>
       {
         "mortgage_value"=>"$200"
